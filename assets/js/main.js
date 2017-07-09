7d2ac16ac37b1c8133d5be83a0eced334f3e3798
Jeff Silberman <jsilberm@gmail.com>
2017-07-09 13:34:27 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,7 +42,7 @@ $(document).ready(function() {
         slides: [
             { src: 'assets/images/hero/hero-1.jpg' },
             { src: 'assets/images/hero/Jerome3.jpg' },
-            { src: 'assets/images/hero/gomez3.jpg' },
+            { src: 'assets/images/hero/gomez4.jpg' },
             { src: 'assets/images/hero/hero-4.jpg' },
             { src: 'assets/images/hero/hero-5.jpg' },
             { src: 'assets/images/hero/grich2.jpg' },
