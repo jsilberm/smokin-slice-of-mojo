b5b5804937f5f55c6c8981fe8bbb5f7b4f90c549
Jeff Silberman <jsilberm@gmail.com>
2017-07-09 13:13:03 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,7 +47,7 @@ $(document).ready(function() {
             { src: 'assets/images/hero/hero-5.jpg' },
             { src: 'assets/images/hero/grich1.jpg' },
             { src: 'assets/images/hero/hero-7.jpg' },
-            { src: 'assets/images/hero/see_cindy_sing.jpg' },
+            { src: 'assets/images/hero/cindy_sings.jpg' },
             { src: 'assets/images/hero/hero-9.jpg' },
             { src: 'assets/images/hero/fd.jpg' },
             { src: 'assets/images/hero/hero-11.jpg' }
