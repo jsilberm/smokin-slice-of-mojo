f9be49f469c49577dfc3a1fba7300ae72e880f7d
Jeff Silberman <jsilberm@gmail.com>
2017-07-09 21:53:42 -0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,7 +38,7 @@ $(document).ready(function() {
         overlay: 'assets/plugins/vegas/dist/overlays/06.png',
         color: '#101113',
         transition: 'zoomOut',
-        transitionDuration: 1000,
+        transitionDuration: 2000,
         slides: [
             { src: 'assets/images/hero/hero-1.jpg' },
             { src: 'assets/images/hero/Jerome3.jpg' },
